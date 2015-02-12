2c5c980c865f508880fad8ea0b5de8ca4daf784f
d1618033 <d1618033@gmail.com>
2015-02-12 06:49:49 +0200

    changed custom and settings form to table

--- projects/maze/index.php
+++ projects/maze/index.php
@@ -31,21 +31,31 @@
     </div>
 
     <div class="form" id="customBox">
-        <p>Time: <input  onchange="onChangeCustomBox()" type="number" min="0" id="custTime" value="120"> seconds</p>
-        <p>Number of columns: <input  onchange="onChangeCustomBox()"  type="number" min="5" value="5" id="custNCols" value="120"></p>
-        <p>Number of rows: <input  onchange="onChangeCustomBox()"  type="number" min="5" value="5" id="custNRows" value="120"></p>
-        <p>Increment: &nbsp; Rows: <input  onchange="onChangeCustomBox()"  type="number" id="incR" min="0" max="10" value="1">
-        &nbsp; Columns: <input  onchange="onChangeCustomBox()" type="number" id="incC" min="0" max="10" value="1"></p>
-        <p> Size of tiles: <input onchange="onChangeCustomBox()" name="size" type="radio" id="big">Big</input>
-        &nbsp; <input onchange="onChangeCustomBox()" name="size" type="radio" id="small" checked="checked">Small</input></p>
-        <p><button onclick="okButton()" id="okButton">Ok</button>&nbsp;<button onclick="cancelButton()" id="cancelButton">Cancel</button>&nbsp;<button onclick="resetToDefault()" id="resetButton">Reset</button></p>
+        <table>
+        <tr><td>Time: <input  onchange="onChangeCustomBox()" type="number" min="0" id="custTime" value="120"> seconds</td></tr>
+        <tr><td>Number of columns: <input  onchange="onChangeCustomBox()"  type="number" min="5" value="5" id="custNCols" value="120"></td></tr>
+        <tr><td>Number of rows: <input  onchange="onChangeCustomBox()"  type="number" min="5" value="5" id="custNRows" value="120"></td></tr>
+        <tr><td>Increment: &nbsp; Rows: <input  onchange="onChangeCustomBox()"  type="number" id="incR" min="0" max="10" value="1">
+        &nbsp; Columns: <input  onchange="onChangeCustomBox()" type="number" id="incC" min="0" max="10" value="1"></td></tr>
+        <tr><td> Size of tiles: <input onchange="onChangeCustomBox()" name="size" type="radio" id="big">Big</input>
+        &nbsp; <input onchange="onChangeCustomBox()" name="size" type="radio" id="small" checked="checked">Small</input></td></tr>
+        <tr>
+        <td><button onclick="okButton()" id="okButton">Ok</button>&nbsp;<button onclick="cancelButton()" id="cancelButton">Cancel</button>&nbsp;<button onclick="resetToDefault()" id="resetButton">Reset</button></td>
+        </tr>
+        </table>
     </div>
     <div class="form" id="settingsBox">
-    <p>Controls:
+    <table>
+    <tr>
+    <td>Controls:
         <input onchange="onChangeSettingsBox()" name="controls" type="radio" id="regular" checked="checked">Regular</input>
         &nbsp; <input onchange="onChangeSettingsBox()" name="controls" type="radio" id="vim">Vim</input>
-    </p>
-    <p><button onclick="okButtonSettings()" id="okButtonSettings">Ok</button>&nbsp;<button onclick="cancelButtonSettings()" id="cancelButtonSettings">Cancel</button>&nbsp;<button onclick="resetToDefaultSettings()" id="resetButtonSettings">Reset</button></p>
+    </td>
+    </tr>
+    <tr>
+    <td><button onclick="okButtonSettings()" id="okButtonSettings">Ok</button>&nbsp;<button onclick="cancelButtonSettings()" id="cancelButtonSettings">Cancel</button>&nbsp;<button onclick="resetToDefaultSettings()" id="resetButtonSettings">Reset</button></td>
+    </tr>
+    </table>
     </div>
     <div id="gameBoard">
         <canvas id="game" height="300" width="1350"/>
