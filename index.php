5b567840a0fceab11bba5899e566bc128fa751ba
d1618033 <d1618033@gmail.com>
2015-02-13 21:14:01 +0200

    create styles dir, moved styles.css to dir, change index.php accordingly

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <? include 'head.php'; ?>
 <title>David's Website</title>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="styles/styles.css">
 </head>
 <body>
 <?
