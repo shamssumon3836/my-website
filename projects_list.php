c6650e53aead000f343fe4f9492772a5c482388f
d1618033 <d1618033@gmail.com>
2015-02-16 19:03:57 +0200

    add github feed

--- projects_list.php
+++ projects_list.php
@@ -4,6 +4,28 @@
 <? include 'head.php'; ?>
 <title>Projects list</title>
 <link rel="stylesheet" type="text/css" href="styles/styles.css">
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+
+google.load("feeds", "1");
+
+function initialize() {
+  var feed = new google.feeds.Feed("http://github.com/d1618033.atom");
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById("feed");
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var div = document.createElement("div");
+        div.innerHTML = entry.content;
+        container.appendChild(div);
+      }
+    }
+  });
+}
+google.setOnLoadCallback(initialize);
+
+</script>
 </head>
 <body>
 <?include './nav_bar.php'?>
@@ -14,7 +36,9 @@
 <li><a href="/projects/random_gauss.php">Random Gaussian Generator</a></li>
 <li><a href="/projects/maze/index.php">Maze Game</a></li>
 </ul>
-<p>For more projects, go to <a href="https://github.com/d1618033">my github</a></p>
+</div>
+<div id="feed">
+<h3>GitHub feed</h3>
 </div>
 </body>
 </html>
