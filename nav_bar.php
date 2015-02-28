181ef879700ff9cd39f8ddee954fc678c808dc0d
d1618033 <d1618033@gmail.com>
2015-02-28 10:41:30 +0200

    add blog to nav bar

--- nav_bar.php
+++ nav_bar.php
@@ -2,6 +2,7 @@
 <ul>
 <li><a href="/">Home</a></li>
 <li><a href="/projects_list.php">Projects</a></li>
+<li><a href="/blog/">Blog</a></li>
 <li><a href="/about_me.php">About me</a></li>
 </ul>
 </div>
