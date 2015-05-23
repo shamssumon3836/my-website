9238b36a070805c82b317a94b9c3f87b75353e7b
d1618033 <d1618033@gmail.com>
2015-05-23 17:32:34 +0300

    indent code

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-<? include 'head.php'; ?>
-<link rel="stylesheet" type="text/css" href="styles/styles.css">
-<title>David's Website</title>
+    <? include 'head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <title>David's Website</title>
 </head>
 <body>
 <?
 include './config.php';
 if ($maintenance == false)
 {
-?>
-<?php include './nav_bar.php'?>
-<div id="content">
-<h2>Welcome to my new website!</h2>
-<p>Feel free to look around...</p>
-</div>
+    ?>
+    <?php include './nav_bar.php'?>
+    <div id="content">
+        <h2>Welcome to my new website!</h2>
+        <p>Feel free to look around...</p>
+    </div>
 <?
 }
 else{
-?>
-<p>Under maintenance...</p>
+    ?>
+    <p>Under maintenance...</p>
 <?
 }
 ?>
--- nav_bar.php
+++ nav_bar.php
@@ -1,8 +1,7 @@
 <div id="navigation_bar">
-<ul>
-<li><a href="/">Home</a></li>
-<li><a href="/projects_list.php">Projects</a></li>
-<li><a href="/blog/">Blog</a></li>
-<li><a href="/about_me.php">About me</a></li>
-</ul>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/projects_list.php">Projects</a></li>
+        <li><a href="/about_me.php">About me</a></li>
+    </ul>
 </div>
